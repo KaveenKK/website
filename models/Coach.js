4bb609a66a4f238418dced15f72237d2fd0b7bae
KaveenKK <161033120+KaveenKK@users.noreply.github.com>
2025-04-17 13:26:01 +0100

    Update Coach.js

--- models/Coach.js
+++ models/Coach.js
@@ -4,6 +4,10 @@ const coachSchema = new mongoose.Schema({
   discord_id: { type: String, unique: true },
   email: String,
   name: String,
+  age: Number,
+  bio: String,
+  specialties: [String],
+  experience: String,
   createdAt: { type: Date, default: Date.now }
 });
 
