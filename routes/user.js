4a59c2f63eebd9796d27c59a1193c7a18e145e12
KaveenKodikarage <kaveenyasas14@gmail.com>
2025-05-04 20:56:51 +0100

    bloody idiot

--- routes/user.js
+++ routes/user.js
@@ -246,7 +246,7 @@ router.get('/status', authMiddleware, async (req, res) => {
  */
 router.get('/user-profile', authMiddleware, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select('discord_id username avatar email xp maples gender');
+    const user = await User.findById(req.user.id).select('discord_id username avatar email xp maples gender physical mental social love career creative travel family style spiritual');
     if (!user) return res.status(404).json({ error: 'User not found' });
     res.json(user);
   } catch (err) {
