779a41d6dfc89aaf85edb244441cc717763d9758
KaveenKodikarage <kaveenyasas14@gmail.com>
2025-05-09 13:36:22 +0100

    Revert "iframe"
    This reverts commit 105adc42cf50c84d3af50f3aee30b24b545c9efb.

--- powerhouses-react/src/index.js
+++ powerhouses-react/src/index.js
@@ -6,31 +6,12 @@ import GroupChat from './GroupChat';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-function getPropsFromUrl() {
-  const params = new URLSearchParams(window.location.search);
-  const houseId = params.get('houseId');
-  const houseName = params.get('houseName');
-  const userId = params.get('userId');
-  const username = params.get('username');
-  const avatar = params.get('avatar');
-  const discord_id = params.get('discord_id');
-  if (houseId && userId) {
-    return {
-      houseId,
-      houseName,
-      user: { _id: userId, username, avatar, discord_id }
-    };
-  }
-  return null;
-}
-
 const root = ReactDOM.createRoot(document.getElementById('reactGroupChatRoot') || document.getElementById('root'));
 
-const chatProps = getPropsFromUrl();
-if (chatProps && document.getElementById('reactGroupChatRoot')) {
+if (window.reactGroupChatProps && document.getElementById('reactGroupChatRoot')) {
   root.render(
     <React.StrictMode>
-      <GroupChat {...chatProps} />
+      <GroupChat {...window.reactGroupChatProps} />
     </React.StrictMode>
   );
 } else {
